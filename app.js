3f9bca17caecfc0f52f40ba9763ac094d8f35d65
sapna-prajapat <95474354+sapna-prajapat@users.noreply.github.com>
2022-03-29 16:52:10 +0530

    first code

---
+++ app.js
@@ -0,0 +1,33 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://root:root@cluster0.vgwni.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+.then(()=>{
+  console.log("Database is connect")
+})
+.catch(err=>{
+  console.log(err)
+})
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../model/userModel.js')
+
+router.post("/signup",(request,response)=>{
+  User.create({
+    username: request.body.username,
+    email: request.body.email,
+    password: request.body.password
+  }).then(result=>{
+    return response.status(201).json(result);
+  })
+  .catch(err=>{
+    return response.status(500).json({error: 'Internal Server Error'});
+  });
+});
+
+router.post('/signin',(request, response)=> {
+  console.log(request.body)
+  User.findOne({email:request.body.email,password:request.body.password})
+    .then(result=>{
+      if(result)
+       return response.status(200).json({status: 'login success', current_user: result});
+      else
+       return response.status(200).json({status: 'login failed'}); 
+    })
+  .catch(err=>{
+    return response.status(500).json({error : 'internal server error'});
+  });
+});
+
+module.exports = router;
