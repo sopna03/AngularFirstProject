87df4059752805a6fb860384426a0aa23847aca6
sapna-prajapat <95474354+sapna-prajapat@users.noreply.github.com>
2022-03-29 18:08:54 +0530

    shubham changes

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,8 @@ var router = express.Router();
 const User = require('../model/userModel.js')
 
 router.post("/signup",(request,response)=>{
-  User.create({
-    username: request.body.username,
-    email: request.body.email,
-    password: request.body.password
-  }).then(result=>{
+  User.create(request.body).then(result=>{
+    console.log('signup success')
     return response.status(201).json(result);
   })
   .catch(err=>{
